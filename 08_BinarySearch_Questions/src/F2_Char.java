059b98352bfe994d8f07488b1a624bcea39f6c90
iyursmilx <iyur.smilx@gmail.com>
2025-08-25 22:17:46 +0530

    Arun

--- 08_BinarySearch_Questions/src/F2_Char.java
+++ 08_BinarySearch_Questions/src/F2_Char.java
@@ -2,25 +2,36 @@ public class F2_Char {
 
     public static void main(String[] args) {
         char target = 'd';
-        char[] arr = {'a','c','d','w', 'z'};
+        char[] arr = { 'a', 'c', 'd', 'w', 'z' };
+        System.out.println(nextGreatestLetter(arr, target));
+        System.out.println(PreviousGreatestLetter(arr, target));
     }
 
     public static char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
-        int end = (letters.length - 1);
+        int end = letters.length - 1;
         while (start <= end) {
-            int mid = start - (end + start) / 2;
-            if (target > letters[mid]) {
-                start = mid + 1;
-            } else if (target < letters[mid]) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
                 end = mid - 1;
-            }
-            if (target == 'z') {
-                return 'a';
             } else {
-                return letters[mid + 1];
+                start = mid + 1;
             }
         }
-        return letters[start];
+        return letters[start % letters.length];
+    }
+
+    public static char PreviousGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > letters[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return letters[end % letters.length];
     }
 }
