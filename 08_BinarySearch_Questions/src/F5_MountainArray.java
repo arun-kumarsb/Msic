cc8be5eb415c035e7fdf69b729dd855df15a325d
iyursmilx <iyur.smilx@gmail.com>
2025-08-26 14:24:09 +0530

    Arun

--- 08_BinarySearch_Questions/src/F5_MountainArray.java
+++ 08_BinarySearch_Questions/src/F5_MountainArray.java
@@ -12,7 +12,7 @@ public class F5_MountainArray {
     // ex: 0,1,0 ans-> 1
     // ex: 0,2,1,0 ans-> 2
     /// 0 1 2 3 4 5 6 7
-/// s-----m-------e
+    /// s-----m-------e
     // [1,2,3,5,6,4,3,2]
     // if e[mid] > e[mid+1] --> we are at decreasing part of array // end = mid
     // if e[mid] < e[mid+1] --> we are at increasing part of array // start = mid+1;
